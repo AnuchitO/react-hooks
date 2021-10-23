4e47fbc96e111306592a83f0bf872ba5d3c66add
AnuchitO <anuchit.prasertasng@gmail.com>
2021-10-23 17:13:03 +0700

    #1: Change language but slow function got call everytime

---
+++ src/memo/Memo.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+
+const slow = (num) => {
+  console.log("slow got called")
+  return num * 2
+}
+
+const ExampleMemo = () => {
+  const [count, setCount] = useState(0)
+  const expensive = slow(count)
+
+  const [thai, setThai] = useState(true)
+  const langs = {
+      lang: thai ? "ไทย" : "English",
+      flag: thai ? '🇹🇭' : '🏴󠁧󠁢󠁥󠁮󠁧󠁿',
+  }
+
+  return (
+    <>
+      <div>expensive: {expensive}</div>
+      <div>number: {count}</div>
+      <button onClick={() => setCount(count + 1)}>Count UP</button>
+      <button onClick={() => setCount(count - 1)}>Count DOWN</button>
+
+      <div>Language: {`${langs.flag} ${langs.lang}`}</div>
+      <button onClick={() => setThai(!thai)}>Change Language</button>
+    </>
+  );
+};
+
+export default ExampleMemo;
+
+
